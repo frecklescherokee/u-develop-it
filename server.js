9b23d21edd63bfd10ffa73d1225c10bbfcb93bf0
frecklescherokee <71302040+frecklescherokee@users.noreply.github.com>
2020-12-24 11:31:43 -0600

    made voting possible

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const db = require('./db/database');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const apiRoutes = require('./routes/apiRoutes');
 // Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Default response for any other request (Not Found)
+// Default response for any other request(Not Found) Catch all other
 app.use((req, res) => {
   res.status(404).end();
 });
